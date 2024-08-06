6419f45195f65463d9852db92e508ecb07f7e34e
Rounak Harde <rounakharde86@gmail.com>
2024-08-07 01:17:17 +0530

    commit in the new branch (payment branch)

---
+++ GitProject01/src/main/java/com/rk/payment/UPIPayment.java
@@ -0,0 +1,10 @@
+package com.rk.payment;
+
+public class UPIPayment {
+	public String doPay(long phoneNumber,double amount)
+	{
+		System.out.println("Payment is done ....");
+		return "Payment is done";
+	}
+
+}
